cc7f1ffaf292b4940bbea31a7ca932bc78227307
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2023-09-14 08:40:47 +0545

    Built-in Directives
    Built-in Directives

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+// For NgFor
+interface Book {
+  name: string;
+  autor: string;
+  image: string;
+}
+
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
@@ -14,28 +21,41 @@ export class BooksComponent {
   // Two way binding
   myname = '';
 
-  name = "Resident Evil"
-  autor = "Krishna"
-  image = "https://wallpapers.com/images/featured/resident-evil-bch1g9v6o0eyn113.jpg"
+  // Also for NgFor
+  books: Book[] = [
+    {
+      name: "Resident Evil",
+      autor: "Krishna",
+      image: "https://wallpapers.com/images/featured/resident-evil-bch1g9v6o0eyn113.jpg"
+    },
+    {
+      name: "Harry Potter",
+      autor: "JK Rowling",
+      image: "https://wallpapers.com/images/featured/resident-evil-bch1g9v6o0eyn113.jpg"
+    },
+    {
+      name: "Adventures of Tom Sawyer",
+      autor: "Mark Twain",
+      image: "https://wallpapers.com/images/featured/resident-evil-bch1g9v6o0eyn113.jpg",
+    },
+    {
+      name: "Alice in Wonderland",
+      autor: "Lewis Carrol",
+      image: "https://wallpapers.com/images/featured/resident-evil-bch1g9v6o0eyn113.jpg",
+    },
+    {
 
-  name1 = "Harry Potter"
-  autor1 = "JK Rowling"
-  image1 = "https://wallpapers.com/images/featured/resident-evil-bch1g9v6o0eyn113.jpg"
+      name: "Ancient Mariner",
+      autor: "Coleridge",
+      image: "https://wallpapers.com/images/featured/resident-evil-bch1g9v6o0eyn113.jpg",
+    },
+    {
+      name: "Arms and the Man",
+      autor: " G.B.Shaw",
+      image: "https://wallpapers.com/images/featured/resident-evil-bch1g9v6o0eyn113.jpg",
+    },
+  ]
 
-
-  name2 = "Adventures of Tom Sawyer"
-  autor2 = "Mark Twain"
-  image2 = "https://wallpapers.com/images/featured/resident-evil-bch1g9v6o0eyn113.jpg"
-
-  name3 = "Alice in Wonderland"
-  autor3 = "Lewis Carrol"
-  image3 = "https://wallpapers.com/images/featured/resident-evil-bch1g9v6o0eyn113.jpg"
-
-  name4 = "Ancient Mariner"
-  autor4 = "Coleridge"
-  image4 = "https://wallpapers.com/images/featured/resident-evil-bch1g9v6o0eyn113.jpg"
-
-  name5 = "Arms and the Man"
-  autor5 = " G.B.Shaw"
-  image5 = "https://wallpapers.com/images/featured/resident-evil-bch1g9v6o0eyn113.jpg"
+  // NgIf directives
+  isShowing = true;
 }
